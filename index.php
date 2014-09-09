30c0284a2038aff098a645a0bda0b21804b41422
Ingrid <Kelder>
2014-09-09 15:03:45 +0300

    Added sample variables

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Harjutus 3</title>
+</head>
+<body>
+	<h1>Muutujad</h1>
+	<?php 
+		$variable = 20;
+		echo "<br>";
+	?>
+	<?php 
+		$variable = 30;
+		echo $variable;
+	?>
+</body>
+</html>
