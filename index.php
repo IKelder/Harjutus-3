182bff1bb6e65036e216336ea31a5358f12b7623
Ingrid <Kelder>
2014-09-09 15:28:16 +0300

    Add sample strigs

--- index.php
+++ index.php
@@ -13,5 +13,18 @@
 		$variable = 30;
 		echo $variable;
 	?>
+	<h2>Stringid</h2>
+	<?php 
+		echo "Hello world!";
+		echo "<br>";
+		echo 'Hello world2!';
+		echo "<br>";
+	?>
+	<?php 
+		$number1 = 24;
+		$number2 = 26;
+		echo "2426";
+	?>
+
 </body>
 </html>
