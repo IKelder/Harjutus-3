b57d4c48ee1155cd011bfe5189e371db7680e235
Ingrid <Kelder>
2014-09-09 16:29:07 +0300

    Arvud

--- index.php
+++ index.php
@@ -90,5 +90,85 @@
 		echo "<p>{$textlenght}</p>";
 	?>
 	<?php echo "A" . trim(" B C D E") . "F"; ?>
+	<h2>Arvud</h2>
+	<?php 
+		$arv1 = 1;
+		$arv2 = 2;
+		echo ($arv1 * $arv2);
+		echo "<br>";
+	?>
+	<?php abs(2); ?>
+	<?php
+    // Viis ruudus:
+    echo pow(5, 2);
+    echo "<br>";
+
+    // Seitse kuubis:
+    echo pow(7, 3);
+    echo "<br>";
+
+    // Kolm astmel seitse:
+    echo pow(3, 7);
+    echo "<br>";
+?>
+<?php
+    // Ruutjuur 25-st:
+    echo sqrt(25);
+    echo "<br>";
+?>
+<?php
+    // Suvaline number
+    echo rand();
+
+    // Suvaline number vahemikus 7 - 22:
+    echo rand(7, 22);
+    echo "<br>";
+?>
+<h3>Numbrite kasvatamine ja kahandamine</h3>
+<?php
+    $sample_nr = 45;
+?>
+<?php
+  $sample_nr = 45;
+  $sample_nr = $sample_nr + 5;
+  echo $sample_nr;
+  echo "<br>";
+?>
+<?php
+    $sample_nr2 = 45;
+    $sample_nr2 += 5;
+    echo $sample_nr2;
+    echo "<br>";
+?>
+<?php
+    $sample_nr2 = 45;
+    $sample_nr2 += 5;
+    echo $sample_nr2;
+?>
+<?php
+    $sample_nr3 = 45;
+    $sample_n3 *= 5;
+    echo $sample_nr3;
+
+    $sample_nr2 = 45;
+    $sample_nr2 /= 5;
+    echo $sample_nr2;
+?>
+<h3>Numbri ja stringi liitmine</h3>
+<?php
+    echo 5 . " teksapüksid";
+    echo "<br>";
+?>
+<?php
+    echo 5 + " teksapüksid";
+    echo "<br>";
+?>
+<?php
+    echo 5 + "5";
+    echo "<br>";
+?>
+<?php
+    echo 5 + "7 teksapüksi";
+?>
 </body>
 </html>
